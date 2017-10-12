cdb047d27dfa04c462d3479b3920ea1cf404b19b
elsajmayer <elsajmayer@gmail.com>
2017-10-12 09:56:19 -0700

    first commit

---
+++ marathon_runner.js
@@ -0,0 +1,9 @@
+var marathonTime = 6;
+
+if (marathonTime >= 4 && marathonTime<=5){
+  console.log("Your time was average.");
+}else if (marathonTime<4){
+  console.log("Your time was excellent!");
+}else {
+  console.log("You need to speed up!")
+}
